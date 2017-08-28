752b24474cdbe50eb20b68d8a76c0b3c7393c872
wangquan84 <wangquan84@126.com>
2017-08-29 00:13:47 +0800

    Resubmit 771cb5e
    1. Add AssetBundle Browser Tool
    2. Add some art resource

---
+++ Assets/Scrips/Examples/02_LoadResources/LoadResources.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class LoadResources : MonoBehaviour 
+{
+    void Awake()
+    {
+        MLResourceManager resMgr = MLResourceManager.Instance;
+        resMgr.InitResourceDefine(new GameResDefine());
+        resMgr.CreateResourceMgr(new ResourcesManager());
+        resMgr.ChangeResourceMgr(ResManagerType.resourceMgr);
+    }
+
+    void Start () 
+    {
+        GameObject carObj = MLResourceManager.Instance.LoadInstance("RoleTex", GameResourceType.RES_ROLE_TEXTURES) as GameObject;
+        carObj.transform.localScale = new Vector3(1.3f, 1.3f, 1f);
+	}
+}
